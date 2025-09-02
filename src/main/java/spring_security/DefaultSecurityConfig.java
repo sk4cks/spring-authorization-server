98f5aba8a83390195a7f540b8abec858074bc861
나승찬 <sk4cks@naver.com>
2025-09-02 23:42:38 +0900

    Spring Authorization Server - 인가서버 시작하기

---
+++ src/main/java/spring_security/DefaultSecurityConfig.java
@@ -0,0 +1,33 @@
+package spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class DefaultSecurityConfig {
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain authSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}1234").authorities("ROLE_USER").build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
